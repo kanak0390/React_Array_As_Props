97b9971712aad50f5dd98fa92dc8230f894a2a44
kanakroy0390 <kanak0390@gmail.com>
2023-02-11 18:27:55 +0530

    C:\Users\Lalan\React\React Array As Props

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../styles/App.css';
+const arr = JSON.parse(window.localStorage.getItem('props') || `["hello","world"]`) // do not change
+const Join = (props) =>{
+  return(
+    <div id ="join">
+      {
+         props.words.join(',')
+      }
+    </div>
+  )
+}
+const App = () => {
+
+  return (
+    <div id="main">
+      <Join words={arr} />
+    </div>
+  )
+}
+
+
+export default App;
